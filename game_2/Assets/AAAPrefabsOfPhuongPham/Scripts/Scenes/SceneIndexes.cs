7d71d6935b1349e3c91db5707a542ddb0c8e0613
PhuongPham1203 <PhuongPham1203@github.com>
2020-07-18 07:02:56 +0700

    Update Transform Scene
    Update transform from scenes

---
+++ game_2/Assets/AAAPrefabsOfPhuongPham/Scripts/Scenes/SceneIndexes.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+public enum SceneIndexes 
+{
+    SCENE_MANAGER = 0,
+    SCENE_PLAYER = 1,
+    SCENE_APOCALYPSE = 2,
+    SCENE_CITY = 3
+}
---
+++ game_2/Assets/AAAPrefabsOfPhuongPham/Scripts/Scenes/SceneManagerOfGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagerOfGame : MonoBehaviour
+{
+    public static SceneManagerOfGame instance;
+    public GameObject loadingScene;
+    public GameObject canvasMainMenu;
+    private void Awake()
+    {
+        instance = this;
+        
+    }
+
+    List<AsyncOperation> sceneLoading = new List<AsyncOperation>();
+    public void LoadGame(){
+        loadingScene.gameObject.SetActive(true);
+        canvasMainMenu.gameObject.SetActive(false);
+
+        sceneLoading.Add( SceneManager.LoadSceneAsync((int)SceneIndexes.SCENE_PLAYER,LoadSceneMode.Additive) );
+        //sceneLoading.Add( SceneManager.LoadSceneAsync((int)SceneIndexes.SCENE_APOCALYPSE,LoadSceneMode.Additive) );
+        //SceneManager.UnloadSceneAsync((int)SceneIndexes.SCENE_MANAGER,LoadSceneMode.Additive);
+
+        
+        StartCoro
+
+    }
+
+    public IEnumerator GetSceneLoadProgress(){
+
+    }
+}
