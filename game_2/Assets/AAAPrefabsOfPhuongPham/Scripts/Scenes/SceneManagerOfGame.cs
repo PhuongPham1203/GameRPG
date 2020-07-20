d77870126a59aab4b1ced964c5d7ff01816a4731
PhuongPham1203 <PhuongPham1203@github.com>
2020-07-20 18:07:31 +0700

    Last commit by PhuongPham 20/07/2020
    last commit before change to anpha

--- game_2/Assets/AAAPrefabsOfPhuongPham/Scripts/Scenes/SceneManagerOfGame.cs
+++ game_2/Assets/AAAPrefabsOfPhuongPham/Scripts/Scenes/SceneManagerOfGame.cs
@@ -127,43 +127,4 @@ public class SceneManagerOfGame : MonoBehaviour
     }
 
 
-    /*
-    public Image loadingBar;
-    public GameObject canvasMainMenu;
-    public GameObject cancasLoadingScene;
-    public Text progressText;
-
-    public void LoadLevel(int sceneIndex)
-    {
-        canvasMainMenu.gameObject.SetActive(false);
-        cancasLoadingScene.gameObject.SetActive(true);
-
-        StartCoroutine(loadAsynchronously(sceneIndex));
-        Debug.Log("done 0");
-
-
-
-
-
-    }
-
-    IEnumerator loadAsynchronously(int sceneIndex)
-    {
-        // ! Loading 0 -> 0.9 && Activation 0.9 -> 1
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-        while (!operation.isDone)
-        {
-
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
-            loadingBar.fillAmount = progress;
-            progressText.text = (progress * 100f).ToString();
-            Debug.Log(progress);
-
-            yield return null;
-        }
-        Debug.Log("done");
-
-    }
-
-    */
 }
