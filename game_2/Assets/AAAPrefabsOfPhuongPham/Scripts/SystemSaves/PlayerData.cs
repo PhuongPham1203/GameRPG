cae070729fc81aee3f0fc964251a15e7db195bd4
PhuongPham1203 <PhuongPham1203@github.com>
2020-07-21 00:22:10 +0700

    Add PlayerData By PhuongPham 21/07/2020
    First push from new BranchOfPhuongPham

---
+++ game_2/Assets/AAAPrefabsOfPhuongPham/Scripts/SystemSaves/PlayerData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+    public int baseHP;
+    public int baseAttackDame;
+    public int basePoseture;
+    public int baseDefend;
+
+    public int level;
+    public int expNow;
+    public int expToLevelUp;
+
+    public int monet;
+    public int soul;
+
+
+
+}
